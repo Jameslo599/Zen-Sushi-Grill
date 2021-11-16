84c704832fe84aa91ebb123d049682ab6e53c517
Jameslo599 <jameslo599@gmail.com>
2021-11-16 03:35:11 -0600

    fixed purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.html"],
+  purge: ["./src/**/*.html",'./src/js/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
